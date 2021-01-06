d6c82a66f4429c7ff1a6e9046155de81aa903c89
Fedor <fedor@DevelopmentOnTheEdge.com>
2021-01-06 20:28:01 +0700

    try to fix

--- source/conf.py
+++ source/conf.py
@@ -28,8 +28,9 @@ author = 'Biosoft.Ru'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
     'sphinxcontrib.kroki',
-    'recommonmark']
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
